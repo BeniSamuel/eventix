ff9621d3de8236c5554bd09ccb0a4a763d9e6134
benisamuel <benisamuel566@gmail.com>
2025-08-07 17:39:17 +0200

    added login and register methods

--- src/main/java/com/evms/www/controller/AuthController.java
+++ src/main/java/com/evms/www/controller/AuthController.java
@@ -1,7 +1,14 @@
 package com.evms.www.controller;
 
+import com.evms.www.dto.LoginDto;
+import com.evms.www.dto.RegisterDto;
+import com.evms.www.model.User;
 import com.evms.www.service.AuthService;
+import com.evms.www.utils.ApiResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
 
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse<User>> registerUser (@RequestBody RegisterDto registerDto) {
+        return authService.registerUser(registerDto);
+    }
 
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<String>> loginUser (@RequestBody LoginDto loginDto) {
+        return authService.loginUser(loginDto);
+    }
 }
