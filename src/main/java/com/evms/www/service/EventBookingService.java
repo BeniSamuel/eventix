e603812cb2ac877f9e4643af86b4770843dc2758
benisamuel <benisamuel566@gmail.com>
2025-08-07 20:18:20 +0200

    added event service handler and event booking service handler

--- src/main/java/com/evms/www/service/EventBookingService.java
+++ src/main/java/com/evms/www/service/EventBookingService.java
@@ -1,4 +1,56 @@
 package com.evms.www.service;
 
+import com.evms.www.dto.BookingDto;
+import com.evms.www.enums.BookingStatus;
+import com.evms.www.enums.Status;
+import com.evms.www.model.Booking;
+import com.evms.www.model.Event;
+import com.evms.www.model.User;
+import com.evms.www.repository.BookingRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class EventBookingService {
+    private final EventService eventService;
+    private final UserService userService;
+    private final BookingRepository bookingRepository;
+
+    public List<Booking> getAllBookings () {
+        return bookingRepository.findAll();
+    }
+
+    public Booking getBookingById (Long id) {
+        return bookingRepository.findById(id).orElse(null);
+    }
+
+    public List<Booking> getBookingByBooker (Long user_id) {
+        User booker = userService.getUserById(user_id);
+        if (booker == null) {
+            return null;
+        }
+
+        return bookingRepository.getBookingsByBooker(booker);
+    }
+
+    public Booking createBooking (BookingDto bookingDto) {
+        Event event = eventService.getEventById(bookingDto.getEvent_id());
+        if (event == null) {
+            return null;
+        }
+
+        User booker = userService.getUserById(bookingDto.getUser_id());
+        if (booker == null) {
+            return null;
+        }
+
+        if (event.getEvent_status().equals(Status.OPENED)) {
+            Booking booking = new Booking(event, booker, BookingStatus.PENDING, bookingDto.getBooked_at());
+            return bookingRepository.save(booking);
+        }
+        return null;
+    }
 }
--- src/main/java/com/evms/www/service/EventService.java
+++ src/main/java/com/evms/www/service/EventService.java
@@ -1,4 +1,75 @@
 package com.evms.www.service;
 
+import com.evms.www.dto.EventDto;
+import com.evms.www.enums.Status;
+import com.evms.www.model.Event;
+import com.evms.www.model.User;
+import com.evms.www.repository.EventRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class EventService {
+    private final EventRepository eventRepository;
+    private final UserService userService;
+
+    public List<Event> getAllEvents () {
+        return this.eventRepository.findAll();
+    }
+
+    public Event getEventById (Long id) {
+        return this.eventRepository.findById(id).orElse(null);
+    }
+
+    public List<Event> getEventsByCreator (Long user_id) {
+        User creator = userService.getUserById(user_id);
+        if (creator != null) {
+            return this.eventRepository.getEventByCreator (creator);
+        }
+        return null;
+    }
+
+    public Event createEvent (EventDto eventDto) {
+        User creator = userService.getUserById(eventDto.getCreator_id());
+        if (creator == null) {
+            return null;
+        }
+
+        Event newEvent = new Event(eventDto.getName(), eventDto.getDescription(), creator, eventDto.getLocation(), eventDto.getOverview(), eventDto.getStart_time(), eventDto.getEnd_time(), Status.OPENED);
+        return eventRepository.save(newEvent);
+    }
+
+    public Event updateEvent (Long id, EventDto eventDto) {
+        Event event = getEventById(id);
+        if (event == null) {
+            return null;
+        }
+
+        User creator = userService.getUserById(eventDto.getCreator_id());
+        if (creator == null) {
+            return null;
+        }
+
+        event.setName(eventDto.getName());
+        event.setDescription(eventDto.getDescription());
+        event.setOverview(eventDto.getOverview());
+        event.setLocation(eventDto.getLocation());
+        event.setRating(eventDto.getRating());
+        event.setStart_time(eventDto.getStart_time());
+        event.setEnd_time(eventDto.getEnd_time());
+
+        return eventRepository.save(event);
+    }
+
+    public Boolean deleteEvent (Long id) {
+        Event event = getEventById(id);
+        if (event != null) {
+            eventRepository.delete(event);
+            return true;
+        }
+        return false;
+    }
 }
