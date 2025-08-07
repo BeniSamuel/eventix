ede5318575cfd11eae04b773a10ab132201fffe0
benisamuel <benisamuel566@gmail.com>
2025-08-07 17:08:14 +0200

    added methods response status

--- src/main/java/com/evms/www/utils/ApiResponse.java
+++ src/main/java/com/evms/www/utils/ApiResponse.java
@@ -1,5 +1,6 @@
 package com.evms.www.utils;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public class ApiResponse <T> {
@@ -13,7 +14,19 @@ public class ApiResponse <T> {
         this.data = data;
     }
 
-//    public static ResponseEntity<ApiResponse<T>> ok (String message, T data) {
-//        return ResponseEntity.ok(new ApiResponse<>(true, message, data));
-//    }
+    public static <T> ResponseEntity<ApiResponse<T>> ok (String message, T data) {
+        return ResponseEntity.ok(new ApiResponse<>(true, message, data));
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> badRequest (String message, T data) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse<>(false, message, data));
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> notFound (String message, T data) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, message, data));
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> created (String message, T data) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(true, message, data));
+    }
 }
