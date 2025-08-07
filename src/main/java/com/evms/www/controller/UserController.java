49bd3f5ce3ca836c45aca1ea5ebd908063261ebe
benisamuel <benisamuel566@gmail.com>
2025-08-07 17:06:45 +0200

    added an improved response object

--- src/main/java/com/evms/www/controller/UserController.java
+++ src/main/java/com/evms/www/controller/UserController.java
@@ -5,7 +5,6 @@ import com.evms.www.model.User;
 import com.evms.www.service.UserService;
 import com.evms.www.utils.ApiResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,40 +18,40 @@ public class UserController {
 
     @GetMapping("/all")
     public ResponseEntity<ApiResponse<List<User>>> getAllUsers () {
-        return ResponseEntity.ok(new ApiResponse<>(true, "Successfully obtained all user!!! 🎉🎉🎉", this.userService.getAllUsers()));
+        return ApiResponse.ok("Successfully obtained all user!!! 🎉🎉🎉", this.userService.getAllUsers());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ApiResponse<User>> getUserById (@PathVariable Long id) {
         User user = this.userService.getUserById(id);
         if (user != null) {
-            return ResponseEntity.ok(new ApiResponse<>(true, "Successfully obtained a user!!! 🎉🎉🎉", user));
+            return ApiResponse.ok("Successfully obtained a user!!! 🎉🎉🎉", user);
         }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, "Failed to obtain a user!!! 💔💔😔", null));
+        return ApiResponse.notFound("Failed to obtain user!!! 💔😔😔", null);
     }
 
     @GetMapping("/email/{email}")
     public ResponseEntity<ApiResponse<User>> getUserByEmail (@PathVariable String email) {
         User user = this.userService.getUserByEmail(email);
         if (user != null) {
-            return ResponseEntity.ok(new ApiResponse<>(true, "Successfully obtained a user!!! 🎉🎉🎉", user));
+            return ApiResponse.ok("Successfully obtained a user!!! 🎉🎉🎉", user);
         }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, "Failed to obtain a user!!! 💔💔😔", null));
+        return ApiResponse.notFound("Failed to obtain user!!! 💔😔😔", null);
     }
 
     @PutMapping("/update/{id}")
     public ResponseEntity<ApiResponse<User>> updateUserById (@PathVariable Long id, @RequestBody RegisterDto registerDto) {
         User user = this.userService.updateUserById(id, registerDto);
         if (user != null) {
-            return ResponseEntity.ok(new ApiResponse<>(true, "Successfully updated a user!!! 🎉🎉🎉", user));
+            return ApiResponse.ok("Successfully updated a user!!! 🎉🎉🎉", user);
         }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, "Failed to update a user!!! 💔💔😔", null));
+        return ApiResponse.notFound("Failed to update user not found!!! 💔😔😔", null);
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<ApiResponse<Boolean>> deleteUserById (@PathVariable Long id) {
         return this.userService.deleteUser(id) ?
-                ResponseEntity.ok(new ApiResponse<>(true, "Successfully deleted a user!!! 🎉🎉🎉", true)) :
-                ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, "Failed to deleted a user!!! 💔💔😔", false));
+                ApiResponse.ok("Successfully deleted a user!!! 🎉🎉🎉", true) :
+                ApiResponse.notFound("Failed to delete a user not found!!! 💔😔😔", false);
     }
 }
