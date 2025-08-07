c5fbe72c70ef0d385550a12166e1162ba01fcb94
benisamuel <benisamuel566@gmail.com>
2025-08-07 21:42:53 +0200

    Updated event repository for easy status event retrieval

--- src/main/java/com/evms/www/repository/EventRepository.java
+++ src/main/java/com/evms/www/repository/EventRepository.java
@@ -1,5 +1,6 @@
 package com.evms.www.repository;
 
+import com.evms.www.enums.Status;
 import com.evms.www.model.Event;
 import com.evms.www.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,5 @@ import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> getEventByCreator (User creator);
+    List<Event> getEventsByEvent_status (Status status);
 }
