8456cddaa64136e907f1937094f218cf5e103878
benisamuel <benisamuel566@gmail.com>
2025-08-10 17:15:29 +0200

    added contribution repository

--- src/main/java/com/evms/www/repository/ContributionRepository.java
+++ src/main/java/com/evms/www/repository/ContributionRepository.java
@@ -1,7 +1,13 @@
 package com.evms.www.repository;
 
 import com.evms.www.model.Contribution;
+import com.evms.www.model.Event;
+import com.evms.www.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ContributionRepository extends JpaRepository<Contribution, Long> {
+    List<Contribution> getContributionsByEvent (Event event);
+    List<Contribution> getContributionsByContributor (User contributor);
 }
