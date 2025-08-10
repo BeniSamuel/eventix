c816f206c4ad809547f122d10edc41fe8d40ef0e
benisamuel <benisamuel566@gmail.com>
2025-08-10 17:12:40 +0200

    added comment repository

--- src/main/java/com/evms/www/repository/CommentRepository.java
+++ src/main/java/com/evms/www/repository/CommentRepository.java
@@ -1,7 +1,13 @@
 package com.evms.www.repository;
 
 import com.evms.www.model.Comment;
+import com.evms.www.model.Event;
+import com.evms.www.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> getCommentsByEvent (Event event);
+    List<Comment> getCommentsByAuthor (User author);
 }
