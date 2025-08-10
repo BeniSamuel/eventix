235ee633259afafb7a045a565fa12d4c570581e5
benisamuel <benisamuel566@gmail.com>
2025-08-10 17:20:07 +0200

    added like repository

--- src/main/java/com/evms/www/repository/LikeRepository.java
+++ src/main/java/com/evms/www/repository/LikeRepository.java
@@ -1,7 +1,13 @@
 package com.evms.www.repository;
 
+import com.evms.www.model.Event;
 import com.evms.www.model.Like;
+import com.evms.www.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LikeRepository extends JpaRepository<Like, Long> {
+    List<Like> getLikesByEvent (Event event);
+    List<Like> getLikesByAuthor (User author);
 }
