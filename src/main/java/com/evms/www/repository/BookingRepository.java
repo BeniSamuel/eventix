260efea13d1ef70922d0d5baf14f6e239b1450b1
benisamuel <benisamuel566@gmail.com>
2025-08-10 17:06:54 +0200

    Added BookingRepository for related work with booking

--- src/main/java/com/evms/www/repository/BookingRepository.java
+++ src/main/java/com/evms/www/repository/BookingRepository.java
@@ -1,5 +1,6 @@
 package com.evms.www.repository;
 
+import com.evms.www.enums.BookingStatus;
 import com.evms.www.model.Booking;
 import com.evms.www.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,5 @@ import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> getBookingsByBooker (User booker);
+    List<Booking> getBookingsByStatus (BookingStatus status);
 }
